1731fac171e3aac0a9588ae8b97dbbce5196abb0
shristi54 <shristi1350@gmail.com>
2020-09-25 16:14:48 +0530

    first commit

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -2,13 +2,12 @@ import React, { Component } from "react";
 
 class UserGreeting extends Component {
   constructor(props) {
-    super(props);
+    super();
 
     this.state = {
       isLoggedIn: true,
     };
   }
-
   render() {
     return (
       // ternary operator----------
@@ -18,12 +17,31 @@ class UserGreeting extends Component {
         <div>Welcome Guest</div>
       )
     );
-    // if else operator
-    // if (this.state.isLoggedIn) return <div>Welcome Vishwas</div>;
-    // else {
-    //   return <div>Welcome Guest</div>;
-    // }
   }
 }
 
 export default UserGreeting;
+
+// constructor(props)
+//   super(props)
+
+//   this.state = {
+//      isLoggedIn: true
+//   };
+
+//   render() {
+//     return (
+//       // ternary operator----------
+//       this.state.isLoggedIn ?
+//         <div>Welcome Vishwas</div> :
+//         <div>Welcome Guest</div>
+
+//     )
+//     // if else operator----------------
+//     // if (this.state.isLoggedIn) return <div>Welcome Vishwas</div>;
+//     // else {
+//     //   return <div>Welcome Guest</div>;
+//     // }
+
+//     };
+// export default UserGreeting;
